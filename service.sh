844b931d818c180f51fb62e018bb42b613b97386
Aliza <alirezanajmabadi79@gmail.com>
2025-11-09 15:21:26 +0330

    use direct path

--- service.sh
+++ service.sh
@@ -1,14 +1,9 @@
 #!/system/bin/sh
 
-MODDIR=${0%/*}
-BINARY="$MODDIR/bin/controller"
-
-# Wait for boot
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 2
 done
 
 sleep 5
 
-# Run completely silent
-[ -x "$BINARY" ] && exec "$BINARY" >/dev/null 2>&1 &
+exec /data/adb/modules/COPG/controller >/dev/null 2>&1 &
