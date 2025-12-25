2f1dfd2018f8d8ccf5bbd2ded6e0900f0a9aacf4
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-12-25 02:33:15 -0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -13,6 +13,8 @@ find_resetprop() {
 
 get_prop_mapping() {
     cat << 'MAPPING'
+TIMESTAMP|ro.build.date.utc|ro.system.build.date.utc|ro.vendor.build.date.utc|ro.system_ext.build.date.utc|ro.vendor_dlkm.build.date.utc|ro.product.build.date.utc|ro.odm.build.date.utc|ro.bootimage.build.date.utc
+INCREMENTAL|ro.build.version.incremental|ro.odm.build.version.incremental|ro.product.build.version.incremental|ro.system.build.version.incremental|ro.system_ext.build.version.incremental|ro.vendor.build.version.incremental|ro.vendor_dlkm.build.version.incremental
 ANDROID_VERSION|ro.build.version.release|ro.build.version.release_or_codename|ro.build.version.release_or_preview_display|ro.odm.build.version.release|ro.odm.build.version.release_or_codename|ro.product.build.version.release|ro.product.build.version.release_or_codename|ro.system.build.version.release|ro.system.build.version.release_or_codename|ro.system_ext.build.version.release|ro.system_ext.build.version.release_or_codename|ro.vendor.build.version.release|ro.vendor.build.version.release_or_codename|ro.vendor_dlkm.build.version.release|ro.vendor_dlkm.build.version.release_or_codenam
 SDK_INT|ro.build.version.sdk|ro.vendor_dlkm.build.version.sdk|ro.vendor.build.version.sdk|ro.system_ext.build.version.sdk|ro.product.build.version.sdk|ro.system.build.version.sdk|ro.odm.build.version.sdk
 BOARD|ro.board.platform|ro.product.board
@@ -38,7 +40,9 @@ while IFS='|' read -r json_key props; do
     [ -z "$json_key" ] && continue
     json_value=$(echo "$json_content" | grep -o "\"$json_key\"[[:space:]]*:[[:space:]]*\"[^\"]*\"" | sed 's/.*:[[:space:]]*"\(.*\)"/\1/')
     if [ -n "$json_value" ]; then
-        if [ "$json_key" = "SDK_INT" ]; then
+        if [ "$json_key" = "TIMESTAMP" ]; then
+            BUILD_DATE="$(LC_ALL=C TZ=UTC date -u -d "@$json_value")"
+        elif [ "$json_key" = "SDK_INT" ]; then
             SDK_FULL="$json_value.0"
         elif [ "$json_key" = "FINGERPRINT" ]; then
             DESCRIPTION="$(echo "$json_value" | awk -F'[:/]' '{print $3"-"$7" "$4" "$5" "$6" "$8}')"
@@ -59,6 +63,12 @@ done < <(get_prop_mapping)
 
 "$resetprop" ro.build.description "$DESCRIPTION"
 "$resetprop" ro.build.flavor "$FLAVOR"
+
+if [ -n "$BUILD_DATE" ]; then
+    for prop in ro.build.date|ro.odm.build.date|ro.product.build.date|ro.system.build.date|ro.system_ext.build.date|ro.vendor.build.date|ro.vendor_dlkm.build.date|ro.bootimage.build.date; then
+        "$resetprop" "$prop" "$BUILD_DATE"
+    done
+fi
 
 if [ -n "$SDK_FULL" ]; then
     for prop in ro.build.version.sdk_full ro.odm.build.version.sdk_full ro.product.build.version.sdk_full ro.system.build.version.sdk_full ro.system_ext.build.version.sdk_full ro.vendor_dlkm.build.version.sdk_full ro.vendor.build.version.sdk_full; then
