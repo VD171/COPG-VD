90de92a1fc8feb51185e6998edd48b0f51d72961
VD $ VD171 @ Priv8 <80125987+VD171@users.noreply.github.com>
2025-12-26 08:12:41 -0300

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -4,15 +4,15 @@ COPG_JSON="/data/adb/COPG.json"
 COPG_ORIGINAL="/data/adb/modules/COPG/original_device.txt"
 COPG_UTILS="/data/adb/modules/COPG/utils.sh"
 
-sh $COPG_UTILS
-sh $COPG_UTILS spoof
+sh "$COPG_UTILS"
+sh "$COPG_UTILS" spoofed
 
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 2
 done
 
 sleep 2
-for FILE in $COPG_JSON $COPG_ORIGINAL; do
-    chmod 0644 $FILE
-    chcon u:object_r:system_file:s0 $FILE
+for FILE in "$COPG_JSON" "$COPG_ORIGINAL"; do
+    chmod 0644 "$FILE"
+    chcon u:object_r:system_file:s0 "$FILE"
 done
